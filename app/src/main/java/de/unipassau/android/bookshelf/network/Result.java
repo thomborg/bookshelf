75c87b8705905fd971b0547769585e6e5266a3f9
Artur Antin <antin.artur@gmail.com>
2019-03-20 16:36:30 +0100

    AsyncTask und Result-Klasse eingefügt sowie Permissons für Internetzugriff.

---
+++ app/src/main/java/de/unipassau/android/bookshelf/network/Result.java
@@ -0,0 +1,62 @@
+package de.unipassau.android.bookshelf.network;
+
+public class Result {
+    private String title, subtitle, publisher, publishedDate, isbn10, authors,
+            thumbnail;
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public String getIsbn10() {
+        return isbn10;
+    }
+
+    public void setIsbn10(String isbn10) {
+        this.isbn10 = isbn10;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
+}
